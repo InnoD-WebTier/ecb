a5c4176ca8dbf25756d7fb7095f90f072ef86339
Benjamin Marimon <bsmarimon@berkeley.edu>
2017-02-26 14:42:05 -0800

    Implement basic search and redo sidebar

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
-import React from 'react'
-import Helmet from 'react-helmet'
-import { Link } from 'react-router'
-import { prefixLink } from 'gatsby-helpers'
-import { config } from 'config'
-import $ from 'jquery'
+import React from 'react';
+import Helmet from 'react-helmet';
+import { Link } from 'react-router';
+import { prefixLink } from 'gatsby-helpers';
+import { config } from 'config';
+import $ from 'jquery';
+import data from './data/bug';
 
 export default class Index extends React.Component {
 
@@ -16,22 +17,34 @@ export default class Index extends React.Component {
   }
 
   componentDidMount() {
+
+    String.prototype.format = function() {
+      var args = arguments;
+      return this.replace(/{(\d+)}/g, function(match, number) {
+        return typeof args[number] != 'undefined'
+          ? args[number]
+          : match
+        ;
+      });
+    };
+
     const loadMap = () => {
-      const uluru = {lat: 37.8713466, lng: -122.2589184};
+      const berkeley_loc = {lat: 37.8709987, lng: -122.2680837};
       const map = new google.maps.Map(document.getElementById('map'), {
         zoom: 15,
-        center: uluru
+        center: berkeley_loc,
+        mapTypeControl: false
       });
-      const markers = [
-        {lat: 37.8713466, lng: -122.2589184, string: "<p>Big memes</p>"},
-        {lat: 37.872, lng: -122.251, string: "<p>Small memes</p>"},
-      ];
-      for (let position of markers) {
+      const bugs = data.bugs;
+      for (let bug of bugs) {
+
+        const desc = "<div class='info-container'><p class='title'>{0}</p><br/><p class='desc'>{1}</p></div>".format(bug.name, bug.string);
+
         const infowindow = new google.maps.InfoWindow({
-          content: position.string,
+          content: desc,
         });
         const marker = new google.maps.Marker({
-          position,
+          position: bug.position,
           map: map
         });
         marker.addListener('click', function() {
@@ -54,7 +67,7 @@ export default class Index extends React.Component {
     const searchTerm = this.state.searchTerm;
     const activeTab = this.state.activeTab;
 
-    const handleSearch = () => {
+    const handleSearch = (e) => {
       this.setState({ searchTerm: e.target.value });
     }
     const goToSearch = () => {
@@ -80,31 +93,42 @@ export default class Index extends React.Component {
         </div>
       );
     }
+
+    const populateData = () => {
+      const searchTerm = this.state.searchTerm.toLowerCase();
+      const filtered = data.bugs.filter(bug => bug.name.toLowerCase().includes(searchTerm));
+      const content = filtered.map((item, i) => (
+          <div key={i} className="result">
+            {item.name}
+          </div>
+      ));
+      return content;
+    }
+
     const genSideBar = () => {
       if (activeTab === "about") {
         return (
           <div className="sidebar-container">
             {tabs()}
-            <p className="info-text">The Berkeley Bug Project tries to make it easier for Berkeley students to collect bugs for assignments. This website is run by the <span className="underline">Etomology Club at Berkeley</span>. If you want to suggest that we add a specific bug to the map, feel free to reach out to us at <span className="underline">ecb@gmail.com</span>. We would love to hear from you!</p>
-            <p className="contact-info">
-              <span className="underline clickable">Facebook</span> <br/><br/>
-              <span className="underline clickable">Twitter</span>
-            </p>
+            <p className="info-text">The Berkeley Bug Project tries to make it easier for students to collect bugs for their assignments or for fun. The website is run by the Etomology Club at Berkeley.<br/><br/> If you think we missed a bug or have some feedback, feel free to reach out to us at <span className="underline">ecb@gmail.com</span>. We would love to hear from you!</p>
           </div>
         );
       } else {
         return (
           <div className="sidebar-container">
             {tabs()}
-            <p className="info-text">Trying to find a specific bug? You can search below and see if it is in our collection!</p>
+            <p className="info-text">Trying to find a specific bug? You can search below and see if it is in our collection.</p>
             <div className="search-container">
-              <img className="search-icon" src="./assets/search-black.png" />
+              <img className="search-icon" src="./assets/search-blue.png" />
               <input
                 id="search-bar"
                 className="search-bar"
                 onChange={handleSearch}
                 value={searchTerm}
               />
+            </div>
+            <div className="results-container">
+              {populateData()}
             </div>
           </div>
         );
@@ -118,9 +142,7 @@ export default class Index extends React.Component {
         />
         <div className="main-container">
           {genSideBar()}
-          <div className="map-container">
-            <div id="map"></div>
-          </div>
+          <div id="map"></div>
         </div>
       </div>
     )
